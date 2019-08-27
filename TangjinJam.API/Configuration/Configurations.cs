d0b8a3cb0ba0d5dc887129314dfb4c5d6b0b2d72
YYong Kim <seungyongkim0327@gmail.com>
2019-08-27 12:10:09 +0900

    Add project files.

---
+++ TangjinJam.API/Configuration/Configurations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TangjinJam.Infrastructure.Ef;
+
+namespace TangjinJam.API.Configuration
+{
+    public static class Configurations
+    {
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config["mysqlconnection:connectionString"];
+            services.AddDbContext<TangjinJamDbContext>(o => o.UseMySQL(connectionString));
+        }
+    }
+}
---
+++ TangjinJam.Infrastructure/Ef/TangjinJamDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TangjinJam.Infrastructure.Ef
+{
+    public class TangjinJamDbContext : DbContext
+    {
+        public TangjinJamDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
